125c92bf62a428e2598f88e8949cee3f612508f6
jasemma <jasmineikpe@gmail.com>
2025-06-01 20:34:50 +0100

    firebase config addition

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ import {
 
 /*---------- Firebase config ---------- */
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MSG_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 /* (Replace every YOUR_* placeholder ↑ with real keys) */
 
